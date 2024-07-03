5f2e7829c09da99c7c025c5f5c82e7e7c17d0823
SherinHartman1098 <156793224+SherinHartman1098@users.noreply.github.com>
2024-07-03 23:39:04 +0200

    v0.0.6

--- data_handler.py
+++ data_handler.py
@@ -12,24 +12,24 @@ class DataHandler:
         self._drop_duplicates_and_na()
         return self.state_data
 
-    def _map_state_abbreviations(self):
-        if self.state_data is not None:
-            states_abbreviation = {
-                "Alabama": "AL", "Alaska": "AK", "Arizona": "AZ", "Arkansas": "AR",
-                "California": "CA", "Colorado": "CO", "Connecticut": "CT", "Delaware": "DE",
-                "District of Columbia": "DC", "Florida": "FL", "Georgia": "GA", "Hawaii": "HI",
-                "Idaho": "ID", "Illinois": "IL", "Indiana": "IN", "Iowa": "IA", "Kansas": "KS",
-                "Kentucky": "KY", "Louisiana": "LA", "Maine": "ME", "Maryland": "MD",
-                "Massachusetts": "MA", "Michigan": "MI", "Minnesota": "MN", "Mississippi": "MS",
-                "Missouri": "MO", "Montana": "MT", "Nebraska": "NE", "Nevada": "NV",
-                "New Hampshire": "NH", "New Jersey": "NJ", "New Mexico": "NM", "New York": "NY",
-                "North Carolina": "NC", "North Dakota": "ND", "Ohio": "OH", "Oklahoma": "OK",
-                "Oregon": "OR", "Pennsylvania": "PA", "Rhode Island": "RI", "South Carolina": "SC",
-                "South Dakota": "SD", "Tennessee": "TN", "Texas": "TX", "Utah": "UT",
-                "Vermont": "VT", "Virginia": "VA", "Washington": "WA", "West Virginia": "WV",
-                "Wisconsin": "WI", "Wyoming": "WY"
-            }
-            self.state_data['State Abbreviation'] = self.state_data['State/Area'].map(states_abbreviation)
+    # def _map_state_abbreviations(self):
+    #     if self.state_data is not None:
+    #         states_abbreviation = {
+    #             "Alabama": "AL", "Alaska": "AK", "Arizona": "AZ", "Arkansas": "AR",
+    #             "California": "CA", "Colorado": "CO", "Connecticut": "CT", "Delaware": "DE",
+    #             "District of Columbia": "DC", "Florida": "FL", "Georgia": "GA", "Hawaii": "HI",
+    #             "Idaho": "ID", "Illinois": "IL", "Indiana": "IN", "Iowa": "IA", "Kansas": "KS",
+    #             "Kentucky": "KY", "Louisiana": "LA", "Maine": "ME", "Maryland": "MD",
+    #             "Massachusetts": "MA", "Michigan": "MI", "Minnesota": "MN", "Mississippi": "MS",
+    #             "Missouri": "MO", "Montana": "MT", "Nebraska": "NE", "Nevada": "NV",
+    #             "New Hampshire": "NH", "New Jersey": "NJ", "New Mexico": "NM", "New York": "NY",
+    #             "North Carolina": "NC", "North Dakota": "ND", "Ohio": "OH", "Oklahoma": "OK",
+    #             "Oregon": "OR", "Pennsylvania": "PA", "Rhode Island": "RI", "South Carolina": "SC",
+    #             "South Dakota": "SD", "Tennessee": "TN", "Texas": "TX", "Utah": "UT",
+    #             "Vermont": "VT", "Virginia": "VA", "Washington": "WA", "West Virginia": "WV",
+    #             "Wisconsin": "WI", "Wyoming": "WY"
+    #         }
+    #         self.state_data['State Abbreviation'] = self.state_data['State/Area'].map(states_abbreviation)
 
     def _drop_unused_columns(self):
         if self.state_data is not None:
